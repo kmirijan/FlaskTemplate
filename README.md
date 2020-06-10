171513e9f072cbdbe9768bb02377cf2e65c5e1d6
Khachatur Mirijanyan <khachaturmirijanyan@gmail.com>
2020-06-10 12:24:24 -0700

    Forms and Routes

--- README.md
+++ README.md
@@ -4,4 +4,8 @@
 set FLASK_APP=flaskblog.py
 set FLASK_DEBUG=1
 
-Debug in terminal vs debug in script
+Debug in terminal vs debug in script (script better)
+
+flask-wtf
+
+
--- flaskblog.py
+++ flaskblog.py
@@ -1,15 +1,45 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'db17c914b0e06f3eb272687c260bcb07' # To stop some kind of cookie manipulation?
+
+posts = [
+	{
+		'author': 'John Doe',
+		'title' : 'Post 1',
+		'content': 'First Post content',
+		'date_posted': 'April 20, 2020'
+	},
+	{
+		'author': 'Jane Doe',
+		'title' : 'Post 2',
+		'content': 'Second Post content',
+		'date_posted': 'April 21, 2020'
+	}
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-	return "<h1>Home Page</h1>"
+	return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-	return "<h1>About Page</h1>"
+	return render_template('about.html', title='About')
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		flash(f'Account created for {form.username.data}!', 'success')
+		return redirect(url_for('home'))
+	return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+	form = LoginForm()
+	return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
